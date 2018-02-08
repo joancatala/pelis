3deaeb7abb744a855651e586ad9996445fcd9402
joan catala <joan@riseup.net>
2018-02-08 12:47:16 +0100

    Avancem. Script funcionant ja

--- organitzador.py
+++ organitzador.py
@@ -6,17 +6,27 @@
 # Modulets
 #
 from os import listdir
+from reportlab.pdfgen import canvas
 
 # Variables
 #
 fitxer = open ( 'fitxer.txt', 'w' ) 
+c = canvas.Canvas("fitxer.pdf")
 contador = 0
+linia = 750
 
 # Llegim el directori de peli i muntem el fitxer txt resultant
 #
 for peli in listdir("/mnt/disc1/joancatala/files/Pelis/"):
-    #print contador, " ", peli
-    fitxer.write(str(contador) + ' ' + peli + '\n') 
-    contador=contador+1
-
+  	fitxer.write(str(contador) + ' ' + peli + '\n') 
+	if linia < 130:
+		linia = 750
+		c.showPage()
+	
+	c.drawString(20, linia, str(contador) + ' ' + peli + '\n')
+    	contador=contador+1
+	linia=linia-20
 fitxer.close()
+
+#c.drawString(20, 750, "hola")
+c.save()
